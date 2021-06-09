3982ae4735c2eeb36d0d056476a1a159f832aa9c
Adedunmol <oyewaleadedunmola@gmail.com>
2021-06-09 15:51:49 +0100

    added chat_page.js

--- Chat/auth.py
+++ Chat/auth.py
@@ -6,10 +6,12 @@ from flask_login import login_user, logout_user, login_required, current_user
 from PIL import Image
 from secrets import token_hex
 import os
-from flask_socketio import send
+from flask_socketio import send, join_room, leave_room
 from time import localtime, strftime
 
 auth = Blueprint('auth', __name__)
+
+ROOMS = ['Fashion', 'Music', 'Games', 'Coding']
 
 
 def save_image(img):
@@ -117,11 +119,26 @@ def users(user_id):
 
 @auth.route('/chat', methods=['GET', 'POST'])
 def chat():
-    return render_template('chat.html', username=current_user.username)
+    return render_template('chat.html', username=current_user.username, room=ROOMS)
 
 
 @socketio.on('message')
 def message(data):
 
     print(f'\n\n{data}\n\n')
-    send({'msg': data['msg'], 'username': data['username'], 'time_stamp': strftime('%b-%d %I:%M%p', localtime())})
+    send({'msg': data['msg'], 'username': data['username'],
+          'time_stamp': strftime('%b-%d %I:%M%p', localtime()), 'room': data['room']})
+
+
+@socketio.on('join')
+def join(data):
+
+    join_room(data['room'])
+    send({'msg': data['username'] + "has joined the " + data['room']}, room=data['room'])
+
+
+@socketio.on('leave')
+def leave(data):
+
+    leave_room(data['room'])
+    send({'msg': data['username'] + "has left the " + data['room']}, room=data['room'])
--- Chat/static/scripts/socketio.js
+++ Chat/static/scripts/socketio.js
@@ -1,20 +1,67 @@
 document.addEventListener('DOMContentLoaded', () => {
     var socket = io('http://' + document.domain + ':' + location.port);
 
+    let room;
+    joinRoom('Fashion');
+
+    // Display incoming messages
     socket.on('message', data => {
         const p = document.createElement('p');
         const span_username = document.createElement('span');
         const span_timestamp = document.createElement('span');
         const br = document.createElement('br');
-        span_username.innerHTML = data.username;
-        span_timestamp.innerHTML = data.time_stamp;
-        p.innerHTML = span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_timestamp.outerHTML;
-        document.querySelector('#display-message-section').append(p);
+
+        if(data.username) {
+            span_username.innerHTML = data.username;
+            span_timestamp.innerHTML = data.time_stamp;
+            p.innerHTML = span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_timestamp.outerHTML;
+            document.querySelector('#display-message-section').append(p);
+        }else {
+            printSysMsg(data);
+        }
     });
 
     // Send message to the server
     document.querySelector('#send_message').onclick = () => {
         socket.send({'msg':document.querySelector('#user_message').value,
-        'username': username});
+        'username': username, 'room': room});
+        // Clear input area
+        document.querySelector('#user_message').value = '';
+    }
+
+    // Room selection
+    document.querySelectorAll('.select-room').forEach(p => {
+        p.onclick = () => {
+            let newRoom = p.innerHTML;
+            if(newRoom == room) {
+                msg = `You are already in ${room}.`
+                printSysMsg(msg);
+            }else {
+                leaveRoom(room);
+                joinRoom(newRoom);
+                room = newRoom;
+            }
+        }
+    });
+
+    // Leave room
+    function leaveRoom(room) {
+        socket.emit('leave', {'username': username, 'room': room});
+    }
+
+    // Join room
+    function joinRoom(room) {
+        socket.emit('join', {'username': username, 'room': room});
+        // Clear message area
+        document.querySelector('#display-message-section').innerHTML = ''
+        // Auto-focus on the text boss
+        document.querySelector('#user_message').focus();
+    }
+
+    // Print system message
+    function printSysMsg(msg) {
+        const p = document.createElement('p');
+        p.innerHTML = msg;
+        document.querySelector('#display-message-section').append(p);
     }
 })
